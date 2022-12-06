7f43b56596d752fd737a8fe423d0fc1737a79375
Azie Melza Pratama <aziemp66@gmail.com>
2022-12-06 21:38:45 +0700

    Setting Cookie

--- main.go
+++ main.go
@@ -2,9 +2,44 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
+
+var jwtKey = []byte("Secret Seklai")
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string
+	jwt.StandardClaims
+}
+
+var users = map[string]interface{}{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+func generateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodEdDSA)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(10 * time.Hour)
+	claims["authorized"] = true
+	claims["user"] = "username"
+
+	tokenString, err := token.SignedString("blablabla")
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
 
 func main() {
 	router := gin.Default()
@@ -13,10 +48,40 @@ func main() {
 		ctx.Header("Content-Type", "application/json")
 	})
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Success!!!",
-		})
+	router.POST("/login", func(ctx *gin.Context) {
+		var credentials Credentials
+
+		ctx.ShouldBindJSON(&credentials)
+
+		expectedPassword, ok := users[credentials.Username]
+
+		if !ok || expectedPassword != credentials.Password {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"code": 403,
+			})
+			return
+		}
+
+		expirationTime := time.Now().Add(time.Minute * 60)
+
+		claims := &Claims{
+			Username: credentials.Username,
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: expirationTime.Unix(),
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, err := token.SignedString(jwtKey)
+
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"code": 500,
+			})
+			return
+		}
+
+		ctx.SetCookie("token", tokenString, expirationTime.Second(), "/", "localhost", false, true)
 	})
 
 	router.Run(":3000")
